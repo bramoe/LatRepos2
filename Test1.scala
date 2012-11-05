6c2ad5170162b8edb5212e882a469d3ffc88014d
bramoe <bramaje_poetra@yahoo.co.id>
2012-11-05 16:33:44 +0700

    Test1.scala

---
+++ Test1.scala
@@ -0,0 +1,16 @@
+object Test1 {
+   def main(args: Array[String]) {
+      println("identitas anggota")
+      println("nama: " )
+      val nama = Console.readLine
+      
+      println("alamat: ")
+      val alamat = Console.readLine
+      
+      println("umur: ")
+      val umur = Console.readLine
+        
+      println("nama: "+nama+ " ,alamat: " +alamat+ " ,umur: "+umur)
+   
+   }
+}
